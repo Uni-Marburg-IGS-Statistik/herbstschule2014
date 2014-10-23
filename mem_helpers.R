64660641fb91aa4adc7c4782c376b2f1bc70dc95
Phillip Alday <phillip.alday@staff.uni-marburg.de>
2014-10-23 15:47:43 +0200

    useful packages for extending lme4 functionality

---
+++ mem_helpers.R
@@ -0,0 +1,14 @@
+# lsmeans
+# pbkrtest
+# LMERConvenienceFunctions
+# lmerTest
+# effects (John Fox)
+# car (John Fox) Anova()
+# mixed
+# blme
+# languageR
+# coefplot2
+
+install.packages("coefplot2",repos="http://www.math.mcmaster.ca/bolker/R",
+                 type="source")
+
